d7de32cb45112f5046da11441472596ffbabc6b6
benjamin1347 <ben.pierce7@gmail.com>
2023-03-07 08:21:45 -0600

    Etch pad finished

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const container = document.querySelector('.container')
 const button = document.querySelector('.size')
 const reset = document.querySelector('.reset')
+const newCol = document.querySelector('.newColor')
+const eraser= document.querySelector('.eraser')
+const blue = document.querySelector('.blue')
 
 
 let area 
 button.addEventListener('click', ()=>{
    area= window.prompt('Enter Size of Board')
-   if(area > 100 || area < 2){
+   if(area > 101 || area < 2){
     alert("UNABLE TO MAKE BOARD!")
    } else {
     makeBoard()
@@ -14,11 +17,12 @@ button.addEventListener('click', ()=>{
    
 })
 
-
+let box
 
 function makeBoard(){
 container.style.gridTemplateColumns = `repeat(${area},1fr)`
 container.style.gridTemplateRows=`repeat(${area},1fr)`
+
 
 let total = area * area
 
@@ -27,18 +31,35 @@ for (let i = 0; i <total ; i++){
     let box = document.createElement('div')
     container.appendChild(box)
     box.className='.box'
-    box.addEventListener('mouseover', ()=>{
-        box.style.backgroundColor='blue';
-        box.style.opacity=".5"
-        
+    newCol.addEventListener('click', ()=> {
+        randomColor(box)
     })
-
+    reset.addEventListener('click', ()=> {
+        box.style.backgroundColor='white'
+    })
+    eraser.addEventListener('click', ()=> {
+        box.addEventListener('mouseover', ()=> {
+            box.style.backgroundColor='white'
+        })
+    })
+    blue.addEventListener('click', ()=> {
+        box.addEventListener('mouseover', ()=> {
+            box.style.backgroundColor = blue.textContent
+        })
+    })
+         
+   
+    
 }
-
 }
-function clearBoard(){
-container.backgroundColor='white'
-}
+function randomColor(elem){
+   
+    let r = Math.floor(Math.random() * 256)
+    let g = Math.floor(Math.random() * 256)
+    let b = Math.floor(Math.random() * 256)
 
-console.log(reset.addEventListener('click',(clearBoard)  
-))
+    elem.addEventListener('mouseover', ()=> {
+        elem.style.backgroundColor= `rgb(${r},${g}, ${b})`
+    })
+    
+}
